7ed9be7d51a94b20cfc48e1d4544aad758fda131
didi-loiola <diego.alves_1996@hotmail.com>
2021-11-12 18:50:49 -0300

    Adicionando o helper formatUrl ao projeto

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,50 +1,52 @@
 const Link = require('../models/link');
+const formatUrl = require('./../helpers/url')
+
 require('dotenv').config();
 
-exports.postEncode = async (req, res, next) => {
+exports.postEncode = async(req, res, next) => {
     try {
-      const generateCode = () => {
-          let text = '';
-          const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-          for(let i=0;i<5;i++){
-            text +=possible.charAt(Math.floor(Math.random()* possible.length))
-          }
-          return text;
+        const generateCode = () => {
+            let text = '';
+            const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            for (let i = 0; i < 5; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length))
+            }
+            return text;
         }
         const url = req.body.url;
-        
-        if(url == "") {
-          return res.status(400).send({error: "URL vazia"});
+
+        if (url == "") {
+            return res.status(400).send({ error: "URL vazia" });
         }
 
         const code = generateCode();
         const resultado = await Link.create({
-          url,
-          code
+            url,
+            code
         })
         res.status(201).send({
             message: 'Link encurtado com sucesso',
-            url : process.env.HOST + resultado.dataValues.code
-        });  
+            url: `${formatUrl(req)}${resultado.dataValues.code}`
+        });
     } catch (error) {
-        res.status(500).send({error: error})
+        res.status(500).send({ error: error })
     }
 }
 
-exports.getDecode = async (req, res, next) => {
-  try {
-    const url = req.body.url;
-    const code = url.substring(22,27);
-    console.log(code)
-  
-    const resultado = await Link.findOne({where:{code}});
-    if(!resultado) return res.sendStatus(404);
-  
-    res.status(200).send({
-      message: 'URL original',
-      url : resultado.dataValues.url
-  });
-  } catch (error) {
-    res.status(500).send({error: error})
-  }
+exports.getDecode = async(req, res, next) => {
+    try {
+        const url = req.body.url;
+        const code = url.substring(22, 27);
+        console.log(code)
+
+        const resultado = await Link.findOne({ where: { code } });
+        if (!resultado) return res.sendStatus(404);
+
+        res.status(200).send({
+            message: 'URL original',
+            url: resultado.dataValues.url
+        });
+    } catch (error) {
+        res.status(500).send({ error: error })
+    }
 }
